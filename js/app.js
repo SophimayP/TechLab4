4c6c61e9d0ec83e3ac315d37fc7b432da421b727
Sophie Price <sophimayp@gmail.com>
2016-05-23 10:38:24 +1200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,73 +3,6 @@ var city;
 var cityUpper;
 //Google Map Key: AIzaSyDZ7402uvsGRTOP_pqKkRm3fjWXbeIJ7pg    
 // Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var geocoder = new google.maps.Geocoder;
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-    geocoder.geocode({'latLng': pos}, function (locations, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        for (var location of locations) {
-          if ($.inArray('locality', location.types) != -1) {
-            cityUpper = location.address_components[2].short_name; //passing the current city to be in this div
-            document.getElementById("barLocDisp").innerHTML = cityUpper;
-            city = location.address_components[2].short_name; //Getting the current city
-            city = city.replace(/\s+/g, '-').toLowerCase(); //making lowercase and any spaces change to - so that it will not break URL and cities like New Plymouth become new-plymouth which works with the URL. 
-              
-            /* -------------- WEATHER ------------ */
-            //Metservice JSON can't seem to be used as has an origin error:  http://metservice.com/publicData/localForecastwellington and Sam Jones was passing it through a proxy which he let me use - hope thats ok as it's the same data I'm receiving all I'm swapping out is the URL and it saves me having to host it on a third party proxy server myself just to get pass the origin not being allowed.
-            var metServ = "http://uni.ey.nz/metservice.php?localObs_";
-            var jsonURL = metServ + city;
-            $.getJSON(jsonURL, function (json) {
-             var clothingLayers = json.threeHour.clothingLayers;
-             $("#1").hide();
-             $("#1to2").hide();
-             $("#2").hide();
-             $("#2to3").hide();
-             $("#3").hide();
-             $("#3to4").hide();
-             if(clothingLayers == "1"){
-                 $("#1").show();
-             }else if(clothingLayers == "1 to 2"){
-                 $("#1to2").show();
-             }else if(clothingLayers == "2"){
-                 $("#2").show();
-             }else if(clothingLayers == "2 to 3"){
-                 $("#2to3").show();
-             }else if(clothingLayers == "3"){
-                 $("#3").show();
-             }else if(clothingLayers == "3 to 4"){
-                 $("#3to4").show();
-             }else{
-                 $("#3to4").show();
-             }
-             var weatherTemp = json.threeHour.temp;
-             var windLayers = json.threeHour.windProofLayers;
-             document.getElementById("barTempDisp").innerHTML = weatherTemp + '°';
-             document.getElementById("weatherDisp").innerHTML = "It's currently " + weatherTemp +" degrees in " + cityUpper + ".";
-             document.getElementById("layersDisp").innerHTML = "MetService recommends " + clothingLayers + " clothing layers and " + windLayers + " windproof layers.";  
-             var rainFall = 5;
-             var rain = false;
-             rainFall = parseInt(json.threeHour.rainfall);
-             if(rainFall >= 5){
-                 rain = true;
-             }
-             if(rain){
-                 document.getElementById("rainDisp").innerHTML = "There's been " + rainFall + "mm of rainfall today so you might want to take a raincoat.";
-             }else{
-                 document.getElementById("rainDisp").innerHTML = "There's been just " + rainFall + "mm of rainfall today so don't worry about a raincoat.";
-             }
-            });
-            break;
-          }
-        }
-      }
-    });
-  });
-}
 
 /* -------------- DATABASE ------------ */
 var data = new Firebase("https://intense-fire-1222.firebaseio.com/");
@@ -177,6 +110,8 @@ function updatePic() {
 
 /*------------------ DISPLAY ---------------------*/
 $(document).ready(function(){
+    getlocation(); //partial callback working geolocation
+	
     //initial hide the three parts of the app
     $("#metservData").hide();
     $("#enterData").hide();
@@ -208,11 +143,84 @@ $(document).ready(function(){
         $("#metservData").hide();
         $("#landingPage").hide();
     });
+    
     aFunct(2, function(bits){
-        console.log(bits);  //callback eg
+        console.log(bits);  //callback understanding experiment
     });
 });
+
+
+function getlocation() {
+   // One-shot position request.
+   navigator.geolocation.getCurrentPosition(onceFoundLocation);   //Finds the position then calls a function
+}
+
+function onceFoundLocation(position) {   //callback function that runs once location is found
+   var geocoder = new google.maps.Geocoder;
+   var pos = {
+     lat: position.coords.latitude,
+     lng: position.coords.longitude
+   };
+   geocoder.geocode({'latLng': pos}, function (locations, status) {
+     if (status == google.maps.GeocoderStatus.OK) {
+       for (var location of locations) {
+         if ($.inArray('locality', location.types) != -1) {
+           cityUpper = location.address_components[2].short_name; //passing the current city to be in this div
+           document.getElementById("barLocDisp").innerHTML = cityUpper;
+           city = location.address_components[2].short_name; //Getting the current city
+           city = city.replace(/\s+/g, '-').toLowerCase(); //making lowercase and any spaces change to - so that it will not break URL and cities like New Plymouth become new-plymouth which works with the URL. 
+             
+           /* -------------- WEATHER ------------ */
+           //Metservice JSON can't seem to be used as has an origin error:  http://metservice.com/publicData/localForecastwellington and Sam Jones was passing it through a proxy which he let me use - hope thats ok as it's the same data I'm receiving all I'm swapping out is the URL and it saves me having to host it on a third party proxy server myself just to get pass the origin not being allowed.
+           var metServ = "http://uni.ey.nz/metservice.php?localObs_";
+           var jsonURL = metServ + city;
+           $.getJSON(jsonURL, function (json) {
+            var clothingLayers = json.threeHour.clothingLayers;
+            $("#1").hide();
+            $("#1to2").hide();
+            $("#2").hide();
+            $("#2to3").hide();
+            $("#3").hide();
+            $("#3to4").hide();
+            if(clothingLayers == "1"){
+                $("#1").show();
+            }else if(clothingLayers == "1 to 2"){
+                $("#1to2").show();
+            }else if(clothingLayers == "2"){
+                $("#2").show();
+            }else if(clothingLayers == "2 to 3"){
+                $("#2to3").show();
+            }else if(clothingLayers == "3"){
+                $("#3").show();
+            }else if(clothingLayers == "3 to 4"){
+                $("#3to4").show();
+            }else{
+                $("#3to4").show();
+            }
+            var weatherTemp = json.threeHour.temp;
+            var windLayers = json.threeHour.windProofLayers;
+            document.getElementById("barTempDisp").innerHTML = weatherTemp + '°';
+            document.getElementById("weatherDisp").innerHTML = "It's currently " + weatherTemp +" degrees in " + cityUpper + ".";
+            document.getElementById("layersDisp").innerHTML = "MetService recommends " + clothingLayers + " clothing layers and " + windLayers + " windproof layers.";  
+            var rainFall = 5;
+            var rain = false;
+            rainFall = parseInt(json.threeHour.rainfall);
+            if(rainFall >= 5){
+                rain = true;
+            }
+            if(rain){
+                document.getElementById("rainDisp").innerHTML = "There's been " + rainFall + "mm of rainfall today so you might want to take a raincoat.";
+            }else{
+                document.getElementById("rainDisp").innerHTML = "There's been " + rainFall + "mm of rainfall today so don't worry about a raincoat.";
+            }
+           });
+           break;
+         }
+       }
+     }
+}
+
 function aFunct(number, callback){
-    var bit = number +1;   //callback eg
+    var bit = number +1;   //callback understanding experiment
     callback(bit);
 };
